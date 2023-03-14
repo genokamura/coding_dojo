fb8acf1906c5a60098aed80aa25360df51606563
genokamura <gen.okamura@tokyo.supersoftware.co.jp>
2023-03-14 21:05:20 +0900

    number test

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -3,10 +3,9 @@ import { fizzbuzz } from "./fizzbuzz";
 describe('fizzbuzz', () => {
   const log = jest.spyOn(console, 'log');
 
-  // testing sample
-  test('sample', () => {
-    fizzbuzz();
+  test('number', () => {
+    fizzbuzz(1);
     expect(console.log).toBeCalled();
-    expect(log.mock.calls[0][0]).toEqual('fizzbuzz');
+    expect(log.mock.calls[0][0]).toEqual('1');
   });
 });
