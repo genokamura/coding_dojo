36689fdc4e43015ece12ea8de2171ddad682a4fb
genokamura <gen.okamura@tokyo.supersoftware.co.jp>
2023-03-14 21:24:43 +0900

    refactoring product code

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,11 +1,14 @@
 export const fizzbuzz = (num: number) => {
   // fizzbuzz
+  const FIZZ = 3;
+  const BUZZ = 5;
+
   for (let i = 1; i <= num; i++) {
-    if (i % 15 === 0) {
+    if (i % (FIZZ * BUZZ) === 0) {
       console.log('fizzbuzz');
-    } else if (i % 3 === 0) {
+    } else if (i % FIZZ === 0) {
       console.log('fizz');
-    } else if (i % 5 === 0) {
+    } else if (i % BUZZ === 0) {
       console.log('buzz');
     } else {
       console.log(i.toString());
