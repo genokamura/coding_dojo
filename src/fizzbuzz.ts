e8c0ba2066cd2efb09d427e1be069387175dbbcb
genokamura <gen.okamura@tokyo.supersoftware.co.jp>
2023-03-14 21:20:40 +0900

    pass test

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -3,6 +3,8 @@ export const fizzbuzz = (num: number) => {
   for (let i = 1; i <= num; i++) {
     if (i % 3 === 0) {
       console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
     } else {
       console.log(i.toString());
     }
